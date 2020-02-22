0656a17620823e1eca2b2d18c6e1516a5fd52c59
Subhankar Maiti <subhankarmaiti40@gmail.com>
2020-02-22 20:05:31 +0530

    grid added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+export default class App extends Component {
+  state = {
+    board: ["", "", "", "", "", "", "", "", ""],
+    turn: "x"
+  };
+  updateState = index => {
+    const { turn, board } = this.state;
+    board[index] = turn;
+    this.setState({ board, turn: turn === "x" ? "o" : "x" });
+  };
+  render() {
+    return (
+      <>
+        <div className="board">
+          {this.state.board.map((item, index) => {
+            return (
+              <div
+                className="square"
+                onClick={() => this.updateState(index)}
+                key={index}
+              >
+                {item}
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;

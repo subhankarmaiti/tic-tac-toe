bb9dfc55f474903cdfc3db16792c8a4489548089
Subhankar Maiti <subhankarmaiti40@gmail.com>
2020-03-11 00:42:41 +0530

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return <div className='App'>Hi This is a homepage</div>;
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require("express");
-const socketIO = require("socket.io");
-const http = require("http");
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 let app = express();
-let server = http.createServer(app);
+let server = http.Server(app);
 let io = socketIO(server);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  app.use(express.static('client/build'));
+  //Express will serve up the index.html file
+  // if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port);
